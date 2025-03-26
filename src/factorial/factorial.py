10c57785e11d618ff6a42f6e61070c1bd8d48571
Brian <brianschell19@gmail.com>
2025-03-26 02:14:05 -0300

    Factorial ahora solicita número si se omite en la línea de comandos

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -5,24 +5,27 @@
 #* Dr.P.E.Colla (c) 2022                                                   *
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
+
 import sys
+
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
         return 0
     elif num == 0: 
         return 1
-        
     else: 
         fact = 1
-        while(num > 1): 
+        while num > 1: 
             fact *= num 
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+# Verificar si se ingresó un número como argumento
+if len(sys.argv) > 1:
+    num = int(sys.argv[1])  # Toma el argumento de la línea de comandos
+else:
+    num = int(input("Ingrese un número para calcular su factorial: "))  # Solicita el número manualmente
+
+print("Factorial ", num, "! es ", factorial(num))
 
